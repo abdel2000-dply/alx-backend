ee766b6a7cbcdc105e33d89c3b28b31e18534bee
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-04 15:01:08 +0100

    task 1: Add Server class for pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+''' simple pagination '''
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''Return a tuple containing the start index and end index
+        '''
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        start_index, end_index = (page - 1) * page_size, page * page_size
+
+        return self.dataset()[start_index:end_index]
