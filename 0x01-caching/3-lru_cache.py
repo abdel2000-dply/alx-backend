bf14f2d28e03dfbd481347b0e890cffdd2c43319
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-08 00:31:52 +0100

    fixing some pycodestyle

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -2,17 +2,7 @@
 ''' LRU Caching
 '''
 from base_caching import BaseCaching
-# You must use self.cache_data - dictionary from the parent class BaseCaching
-# You can overload def __init__(self): but don’t forget to call the parent init: super().__init__()
-# def put(self, key, item):
-#   Must assign to the dictionary self.cache_data the item value for the key key.
-#   If key or item is None, this method should not do anything.
-#   If the number of items in self.cache_data is higher that BaseCaching.MAX_ITEMS:
-#       you must discard the least recently used item (LRU algorithm)
-#       you must print DISCARD: with the key discarded and following by a new line
-# def get(self, key):
-#   Must return the value in self.cache_data linked to key.
-#   If key is None or if the key doesn’t exist in self.cache_data, return None.
+
 
 class LRUCache(BaseCaching):
     ''' LRU cache class
@@ -30,7 +20,7 @@ class LRUCache(BaseCaching):
             discard = self.order.pop(0)
             del self.cache_data[discard]
             print('DISCARD:', discard)
-        
+
         if key in self.cache_data:
             self.order.remove(key)
         self.order.append(key)
