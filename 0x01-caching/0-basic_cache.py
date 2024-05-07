aeb342fbc654e17833b81b7a21642022582e0b16
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-07 21:07:27 +0100

    task 0: BasicCache class to inherit from BaseCaching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,2 +1,22 @@
 #!/usr/bin/env python3
+'''Basic dictionary
+'''
+from base_caching import BaseCaching
 
+
+class BasicCache(BaseCaching):
+    ''' BasicCache
+    '''
+    def put(self, key, item):
+        ''' Add an item in the cache
+        '''
+        if key and item:
+            self.cache_data[key] = item
+        return None
+
+    def get(self, key):
+        ''' Get an item by key
+        '''
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
