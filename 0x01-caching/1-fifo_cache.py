a704975f21fa51351396ee888efe53544fa56a2b
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-07 22:57:32 +0100

    task 1: Add FIFO cache implementation

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+'''FIFO cache'''
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    '''FIFO cache class
+    '''
+    def __init__(self):
+        '''Constructor
+        '''
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        '''Add an item in the cache
+        '''
+        if key is None or item is None:
+            return None
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if key not in self.cache_data:
+                discard = self.keys.pop(0)
+                del self.cache_data[discard]
+                print('DISCARD:', discard)
+        self.keys.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        '''Get an item by key
+        '''
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
