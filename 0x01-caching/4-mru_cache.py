fd0745f89d7f79e423c20c13b25393427e430845
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-08 02:07:11 +0100

    fixing pycodestyle

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -2,17 +2,7 @@
 ''' MRU Caching
 '''
 from base_caching import BaseCaching
-# ou must use self.cache_data - dictionary from the parent class BaseCaching
-# You can overload def __init__(self): but don’t forget to call the parent init: super().__init__()
-# def put(self, key, item):
-#   Must assign to the dictionary self.cache_data the item value for the key key.
-#   If key or item is None, this method should not do anything.
-#   If the number of items in self.cache_data is higher that BaseCaching.MAX_ITEMS:
-#       you must discard the most recently used item (MRU algorithm)
-#       you must print DISCARD: with the key discarded and following by a new line
-# def get(self, key):
-#   Must return the value in self.cache_data linked to key.
-#   If key is None or if the key doesn’t exist in self.cache_data, return None.
+
 
 class MRUCache(BaseCaching):
     ''' MRU cache class
