4fd91546d5f29135f57946dc91ed576a06625791
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-15 02:07:33 +0100

    update

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -2,7 +2,7 @@
 '''Parametrize templates
 '''
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
